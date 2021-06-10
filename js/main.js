64df28f2c23867870a282bee0e12027ab4fcbe32
Kravec-mary <76391242+Kravec-mary@users.noreply.github.com>
2021-06-10 14:09:28 +0300

    Правки больше деталей

--- js/main.js
+++ js/main.js
@@ -16,38 +16,85 @@ const checkLengthLine = (line, length) => (line.length <= length);
 
 checkLengthLine('Проверка длины строки', 140);
 
-
-
-const ID = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 ];
-
-const URL = [
-  'photos/1.jpg',
-  'photos/2.jpg',
-  'photos/3.jpg',
-  'photos/4.jpg',
-  'photos/5.jpg',
-  'photos/6.jpg',
-  'photos/7.jpg',
-  'photos/8.jpg',
-  'photos/9.jpg',
-  'photos/10.jpg',
-  'photos/11.jpg',
-  'photos/12.jpg',
-  'photos/13.jpg',
-  'photos/14.jpg',
-  'photos/15.jpg',
-  'photos/16.jpg',
-  'photos/17.jpg',
-  'photos/18.jpg',
-  'photos/19.jpg',
-  'photos/20.jpg',
-  'photos/21.jpg',
-  'photos/22.jpg',
-  'photos/23.jpg',
-  'photos/24.jpg',
-  'photos/25.jpg',
+//4 раздел
+const COUNT = 25;
+const MAX_COMMENTS = 5;
+const COMMENTS_MESSAGE = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.',
+  'В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+const COMMENTS_NAME = [
+  'Артём',
+  'Мила',
+  'Никита',
+  'Олеся',
+  'Святослав',
+  'Кирилл',
 ];
 
+const COMMENTS_AVATAR = [
+  'img/avatar-1.svg',
+  'img/avatar-2.svg',
+  'img/avatar-3.svg',
+  'img/avatar-4.svg',
+  'img/avatar-5.svg',
+  'img/avatar-6.svg',
+];
+
+const Likes = {  // enum
+  MIN: 15,
+  MAX: 200,
+};
+
+const Id = {
+  MIN: 1,
+  MAX: 100,
+};
+
+const DEDCRIPTION_PHOTO = 'Это мой первый пост в кекстаграме';
+
+const photos = [];
+
+const getRandomInt = (elements) => {
+ return elements[_.random(0, elements.length - 1)],
+};
+
+const addComments = () => {
+  const comments = [];
+
+  for (let i = 0; i < getRandomInt(0, MAX_COMMENTS); i++) {
+    comments.push({
+      id: getRandomInt(Id.MIN, Id.MAX),
+      avatar: COMMENTS_AVATAR[getRandomInt(0, COMMENTS_AVATAR.length -1)],
+      message: COMMENTS_MESSAGE[getRandomInt(0, COMMENTS_MESSAGE.length -1)],
+      name: COMMENTS_NAME[getRandomInt(0, COMMENTS_NAME.length - 1)],
+    });
+  }
+
+  return comments;
+};
+
+const addPhoto = (index) => ({
+  id: index,
+  url: `photos/${index}.jpg`,
+  description: DEDCRIPTION_PHOTO,
+  likes: getRandomInt(Likes.MIN, Likes.MAX),
+  comments: addComments(),
+});
+
+const addPhotos = () => {
+  for (let i = 0; i < COUNT; i++) {
+    photos.push(addPhoto(i));
+  }
+};
+addPhotos();
+
+/*
 const DESCRIPTION = [
   'Пляж с лежаками и белой галькой',
   'Как пройти к пляжу',
@@ -75,76 +122,4 @@ const DESCRIPTION = [
   'Мы на концерте, всем пис',
   'Сафари по Танзании',
 ];
-
-const getRandomLikes = (min, max) => {
-  if (min < 0 || max < 0) {
-    return -1;
-  }
-
-  if (min > max) {
-    [min, max] = [max, min];
-  }
-
-  return Math.round(Math.random() * (max - min)) + min;
-};
-
-getRandomLikes (15, 200);
-
-
-const COMMENTS_AVATAR = [
-  'img/avatar-1.svg',
-  'img/avatar-2.svg',
-  'img/avatar-3.svg',
-  'img/avatar-4.svg',
-  'img/avatar-5.svg',
-  'img/avatar-6.svg',
-];
-
-const COMMENTS_MESSAGE = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.',
-  'В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
-];
-
-const COMMENTS_NAME = [
-  'Артём',
-  'Мила',
-  'Никита',
-  'Олеся',
-  'Святослав',
-  'Кирилл',
-];
-
-const getRandomArrayElement = (elements) => {
-  return elements[_.random(0, elements.length - 1)];
-};
-
-const getComments = () => {
- return {
-   id: const getRandomCommentsId = (min, max) => {
-    if (min < 0 || max < 0) {
-      return -1;
-    }
-
-    if (min > max) {
-      [min, max] = [max, min];
-    }
-
-    return Math.round(Math.random() * (max - min)) + min;
-  };
-
-  getRandomCommentsId (100, 300);
-
-   avatar: getRandomArrayElement(COMMENTS_AVATAR),
-   message: getRandomArrayElement(COMMENTS_MESSAGE),
-   name: getRandomArrayElement(COMMENTS_NAME),
-  };
-};
-
-const newComments = [
-  getComments(),
-];
+*/
